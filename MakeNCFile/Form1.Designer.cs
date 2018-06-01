9b2740a077c7b5a1e8c92fbe6ce341a9495b8312
hch <chhong@yougar.com>
2018-06-01 16:08:37 +0900

    초기 커밋.

---
+++ MakeNCFile/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace MakeNCFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnPanelClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtAddCmd = new System.Windows.Forms.TextBox();
+            this.btnInsertCmd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 500);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(679, 16);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 21);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "2";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(635, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "간격 :";
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(735, 8);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 34);
+            this.btnView.TabIndex = 5;
+            this.btnView.Text = "GCode";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(816, 8);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 34);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(102, 26);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // btnPanelClear
+            // 
+            this.btnPanelClear.Location = new System.Drawing.Point(537, 9);
+            this.btnPanelClear.Name = "btnPanelClear";
+            this.btnPanelClear.Size = new System.Drawing.Size(75, 33);
+            this.btnPanelClear.TabIndex = 9;
+            this.btnPanelClear.Text = "Clear";
+            this.btnPanelClear.UseVisualStyleBackColor = true;
+            this.btnPanelClear.Click += new System.EventHandler(this.btnPanelClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(823, 517);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(68, 34);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(637, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(254, 460);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // txtAddCmd
+            // 
+            this.txtAddCmd.Location = new System.Drawing.Point(637, 525);
+            this.txtAddCmd.Name = "txtAddCmd";
+            this.txtAddCmd.Size = new System.Drawing.Size(59, 21);
+            this.txtAddCmd.TabIndex = 13;
+            this.txtAddCmd.Text = "Z10";
+            this.txtAddCmd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnInsertCmd
+            // 
+            this.btnInsertCmd.Location = new System.Drawing.Point(703, 517);
+            this.btnInsertCmd.Name = "btnInsertCmd";
+            this.btnInsertCmd.Size = new System.Drawing.Size(54, 34);
+            this.btnInsertCmd.TabIndex = 14;
+            this.btnInsertCmd.Text = "Insert";
+            this.btnInsertCmd.UseVisualStyleBackColor = true;
+            this.btnInsertCmd.Click += new System.EventHandler(this.btnInsertCmd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(763, 517);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(54, 34);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 563);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsertCmd);
+            this.Controls.Add(this.txtAddCmd);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPanelClear);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Make NC File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.Button btnPanelClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtAddCmd;
+        private System.Windows.Forms.Button btnInsertCmd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ MakeNCFile/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MakeNCFile
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        List<CLICKPOS> pos = new List<CLICKPOS>();
+        List<CLICKPOS> pos2 = new List<CLICKPOS>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) // 왼쪽 버튼 클릭이면 pos에 좌표 추가
+            {
+                CLICKPOS cp = new CLICKPOS();
+                cp.X = e.X / 10;
+                cp.Y = e.Y / 10;
+                pos.Add(cp);
+            }
+            else if (e.Button == MouseButtons.Right) // 오른쪽 버튼 클릭이면 pos에서 좌표 제거
+            {
+                int x = e.X / 10;
+                int y = e.Y / 10;
+                for (int i = 0; i < pos.Count; i++)
+                {
+                    if (pos[i] != null && pos[i].X == x && pos[i].Y == y)
+                    {
+                        g.FillRectangle(Brushes.White, pos[i].X * 10 + 1, pos[i].Y * 10 + 1, 9, 9);
+                        pos[i] = null;  // null로 만듬!!! pos 좌표 체크할때 null 여부 체크할것
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pos.Count > 0)
+            {
+                //textBox2.Text = pos.Count.ToString();
+
+                foreach (var p in pos)
+                {
+                    if (p != null)
+                    {
+                        g.FillRectangle(Brushes.Blue, p.X * 10 + 1, p.Y * 10 + 1, 9, 9); // 파란색 칸 채우기
+                    }
+                }
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mLeftDown)
+            {
+                int x = e.X / 10;
+                int y = e.Y / 10;
+
+                pos.Add(new CLICKPOS() { X = x, Y = y });
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // 테두리
+            g.DrawRectangle(Pens.Black, 0, 0, panel1.Width - 1, panel1.Height - 1);
+
+            // 그리드라인
+            for (int y = 10; y < panel1.Height; y += 10) g.DrawLine(Pens.Black, 0, y, panel1.Width, y);
+            for (int x = 10; x < panel1.Width; x += 10) g.DrawLine(Pens.Black, x, 0, x, panel1.Height);
+        }
+
+        bool mLeftDown = false;
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mLeftDown = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mLeftDown = false;
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            List<CLICKPOS> pos_copy = pos;
+            Int32[,] kk = new Int32[panel1.Width / 10, panel1.Height / 10];
+
+            if (MessageBox.Show("순서대로 저장하시겠습니까?", "Save", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                foreach (var p in pos_copy)
+                {
+                    if(p != null) kk[p.X, p.Y] = 1;
+                }
+
+                pos_copy.Clear();
+
+                for (int y = 0; y < panel1.Height / 10; y++)
+                {
+                    for (int x = 0; x < panel1.Width / 10; x++)
+                    {
+                        if (kk[x, y] == 1) pos_copy.Add(new CLICKPOS() { X = x, Y = y });
+                    }
+                }
+            }
+
+            double inc = Double.Parse(textBox3.Text);
+
+            for (int i = pos_copy.Count - 1; i >= 0; i--)
+            {
+                if (pos_copy[i] == null) pos_copy.RemoveAt(i);
+            }
+
+            richTextBox1.Clear();
+
+            List<CLICKPOS> pos2 = new List<CLICKPOS>();
+            bool equal = false;
+            foreach (var p in pos_copy)
+            {
+                equal = false;
+                if (pos2.Count == 0) pos2.Add(p);
+                else
+                {
+                    for (int i = 0; i < pos2.Count; i++)
+                    {
+                        if (p.X == pos2[i].X && p.Y == pos2[i].Y)
+                        {
+                            equal = true;
+                            break;
+                        }
+                    }
+
+                    if (equal == false) pos2.Add(p);
+                }
+            }
+
+            //for (int i = 0; i < pos_copy.Count; i++)
+            //{
+            //    richTextBox1.Text += "G11 " + pos_copy[i].X.ToString() + " , " + pos_copy[i].Y.ToString() + "\r\n";
+            //}
+
+            //richTextBox1.Text += "\r\n------------------------------------------ \r\n\r\n";
+
+
+            richTextBox1.Text += "YouGar Mode File - YHWH\r\n";
+
+            for (int i = 0; i < pos2.Count; i++)
+            {
+                richTextBox1.Text += "G11 X" + (pos2[i].X * inc).ToString() + "  Y" + (pos2[i].Y * inc).ToString() + "\r\n";
+            }
+
+            richTextBox1.Text += "M02\r\n";
+
+            //richTextBox1.Text += "\r\n------------------------------------------ \r\n\r\n";
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPanelClear_Click(object sender, EventArgs e)
+        {
+            pos.Clear();
+            g.FillRectangle(Brushes.White, 0, 0, panel1.Width - 1, panel1.Height - 1);
+
+            // 테두리
+            g.DrawRectangle(Pens.Black, 0, 0, panel1.Width - 1, panel1.Height - 1);
+
+            // 그리드라인
+            for (int y = 10; y < panel1.Height; y += 10) g.DrawLine(Pens.Black, 0, y, panel1.Width, y);
+            for (int x = 10; x < panel1.Width; x += 10) g.DrawLine(Pens.Black, x, 0, x, panel1.Height);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string name = "NCFile_" + DateTime.Now.ToString("yyyymmdd_HHmmss") + ".nc";
+            saveFileDialog1.FileName = name;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //File.WriteAllText(saveFileDialog1.FileName, textBox1.Text.Trim());
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text.Replace("\n", "\r\n").Trim());
+
+                MessageBox.Show("저장 완료");
+            }
+        }
+
+        private void btnInsertCmd_Click(object sender, EventArgs e)
+        {
+            string temp = string.Empty;
+            foreach (var s in richTextBox1.Lines)
+            {
+                if (s.Trim().StartsWith("G") || s.Trim().StartsWith("g"))
+                {
+                    temp += s.Trim() + "  " + txtAddCmd.Text.Trim() + "\r\n";
+                }
+                else
+                {
+                    temp += s.Trim() + "\r\n";
+                }
+            }
+
+            richTextBox1.Text = temp;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string temp = string.Empty;
+            foreach (var s in richTextBox1.Lines)
+            {
+                if (s.Trim().IndexOf(txtAddCmd.Text.Trim()) > -1)
+                {
+                    temp += s.Replace(txtAddCmd.Text.Trim(), "").Trim() + "\r\n";
+                }
+                else
+                {
+                    temp += s.Trim() + "\r\n";
+                }
+            }
+
+            richTextBox1.Text = temp;
+        }
+    }
+
+    public class CLICKPOS
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
